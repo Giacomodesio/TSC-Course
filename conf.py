3014c7dc6d1249178ea1b93002579dfb05e2b120
Giacomodesio <giacomo.desio16@gmail.com>
2025-05-15 13:57:25 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,37 @@
-project = 'TSC_course'
-copyright = '2024, Author'
-author = 'De Sio'
+# Configuration file for the Sphinx documentation builder.
+# For full documentation: https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))  # Adatta il path alla root del progetto
+
+# -- Project information -----------------------------------------------------
+project = 'TSC_course'
+copyright = '2024, De Sio'
+author = 'De Sio'
+release = '1.0.0'  # Versione del progetto
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'autoapi.extension',
+    'sphinx.ext.mathjax'
+]
+
+autoapi_type = 'python'
+autoapi_dirs = ['../../']  # Root directory del codice Python
+autoapi_ignore = ['*conf.py']
+autoapi_generate_api_docs = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for AutoAPI -----------------------------------------------------
+autoapi_options = [
+    'members',
+    'undoc-members',
+    'show-inheritance',
+]
